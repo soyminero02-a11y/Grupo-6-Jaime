5db046ebc0506fe1cd352a7d19aeb76c020d7492
zmg <zohair.mouhim@alu.uhu.es>
2026-03-18 08:55:38 +0100

    feat: añadir pipeline de CI inicial

---
+++ src/main/java/com/mgcss/track/MgcssTrackApplication.java
@@ -0,0 +1,13 @@
+package com.mgcss.track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgcssTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgcssTrackApplication.class, args);
+	}
+
+}
